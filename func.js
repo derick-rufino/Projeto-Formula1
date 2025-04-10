698b1737af9f866066ea32063733f80419b65ed1
derick-rufino <derick10011@gmail.com>
2025-04-10 13:03:08 -0300

    Informações adicionadas
    - Adicionados links nos cards iniciais e asides
    - Definida Estrutura padrão da main dos pilotos
    - Correções Ortográficas

--- func.js
+++ func.js
@@ -1,5 +1,19 @@
-
-
 function sennaSection(){
 window.location.assign("/pilotos/senna.html");
+}
+
+function fittipaldiSection(){
+    window.location.assign("/pilotos/fittipaldi.html");
+}
+
+function barrichelloSection(){
+    window.location.assign("/pilotos/barrichello.html");
+}
+
+function piquetSection(){
+    window.location.assign("/pilotos/piquet.html");
+}
+
+function paceSection(){
+    window.location.assign("/pilotos/pace.html");
 }
